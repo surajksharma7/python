14ac1185ee8342b2d6f4a0cb9a29b9e9770d4439
suraj kumar <140508798+surajksharma7@users.noreply.github.com>
2024-08-31 20:52:46 +0530

    initial commit

---
+++ 01_python.py
@@ -0,0 +1,6 @@
+print("my name is Suraj Kumar") # this  will print name
+ 
+print("i amd student of Mca first year") # code to print course name
+
+import sys # importing system library
+print("python version info : ",sys.version_info) # to print python version 
--- hey.py
+++
@@ -1,4 +0,0 @@
-# D:\Learning\python\mylibrary\__init__.py
-
-from .math_ops import add, subtract
-from .string_ops import to_uppercase, to_lowercase
